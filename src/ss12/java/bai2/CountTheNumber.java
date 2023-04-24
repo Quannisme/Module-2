1b8da0ccf8756e0c58d38eddb340679871e5e9a5
quanisme <quanismevn@gmail.com>
2023-04-24 09:19:26 +0700

    2

--- src/ss12/java/bai2/CountTheNumber.java
+++ src/ss12/java/bai2/CountTheNumber.java
@@ -6,23 +6,21 @@ import java.util.Scanner;
 
 public class CountTheNumber {
     public static void main(String[] args) {
-        Map<String ,Integer>myMap=new HashMap<>();
-        Scanner sc=new Scanner(System.in);
+        Map<String, Integer> myMap = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
         System.out.println("Nhập chuỗi :");
-        String temp=sc.nextLine();
-        String arr[]=temp.split("\\s");
+        String temp = sc.nextLine();
+        String arr[] = temp.split("\\s");
         for (int i = 0; i < arr.length; i++) {
-            if(myMap.containsKey(arr[i]))
-            {
-                int count=myMap.get(arr[i])+1;
-                myMap.put(arr[i],count);
-            }else {
-                myMap.put(arr[i],1);
+            if (myMap.containsKey(arr[i])) {
+                int count = myMap.get(arr[i]) + 1;
+                myMap.put(arr[i], count);
+            } else {
+                myMap.put(arr[i], 1);
             }
         }
-        for(String w: myMap.keySet())
-        {
-            System.out.println(w+myMap.get(w));
+        for (String w : myMap.keySet()) {
+            System.out.println(w + myMap.get(w));
         }
     }
 }
